117ecc7f90d76e4fcc8382d6a30958fda6a22fb6
xwshiba <xw2357@columbia.edu>
2024-04-12 14:46:40 -0700

    abstracted services.js functions

--- src/services.js
+++ src/services.js
@@ -1,178 +1,102 @@
 // This file contains the fetch services to my own backend
 // Some infos are redudant and can be abstract out
-export function fetchAlbumReviews(id) {
-    return fetch(`/api/v1/albums/${id}/reviews`, {
-        method: 'GET',
-        headers: {
-            'content-type': 'application/json'
-        },
-    })
+
+// variables
+const api = '/api/v1';
+const headers = {
+    'content-type': 'application/json',
+};
+
+
+// fetchRequest is a helper function to handle fetch requests
+function fetchRequest(url, options) {
+    return fetch(url, options)
         .catch(() => Promise.reject({ error: 'network-error' }))
         .then(response => {
             if (response.ok) {
                 return response.json();
-            };
+            }
             return response.json()
                 .catch(error => Promise.reject({ error }))
                 .then(err => Promise.reject(err));
         });
+};
+
+
+// All backend fetch services below
+export function fetchAlbumReviews(id) {
+    return fetchRequest(`${api}/albums/${id}/reviews`, {
+        method: 'GET',
+        headers: headers,
+    });
 };
 
 export function fetchSession() {
-    return fetch('/api/v1/session', {
+    return fetchRequest(`${api}/session`, {
         method: 'GET',
-        headers: {
-            'content-type': 'application/json'
-        },
+        headers: headers,
         credentials: 'include',
-    })
-        .catch(() => Promise.reject({ error: 'network-error' }))
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            };
-            return response.json()
-                .catch(error => Promise.reject({ error }))
-                .then(err => Promise.reject(err));
-        });
+    });
 };
 
 export function fetchLogin(username) {
-    return fetch('/api/v1/session', {
+    return fetchRequest(`${api}/session`, {
         method: 'POST',
-        headers: {
-            'content-type': 'application/json'
-        },
+        headers: headers,
         credentials: 'include',
         body: JSON.stringify({ username }),
-    })
-        .catch(() => Promise.reject({ error: 'network-error' }))
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            };
-            return response.json()
-                .catch(error => Promise.reject({ error }))
-                .then(err => Promise.reject(err));
-        });
+    });
 };
 
 export function fetchLogout() {
-    return fetch('/api/v1/session', {
+    return fetchRequest(`${api}/session`, {
         method: 'DELETE',
-    })
-        .catch(() => Promise.reject({ error: 'network-error' }))
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            };
-            return response.json()
-                .catch(error => Promise.reject({ error }))
-                .then(err => Promise.reject(err));
-        });
+    });
 };
 
 export function fetchUserLibrary() {
-    return fetch('/api/v1/userLibrary', {
+    return fetchRequest(`${api}/userLibrary`, {
         method: 'GET',
-        headers: {
-            'content-type': 'application/json',
-        },
+        headers: headers,
         credentials: 'include',
     })
-        .catch(err => Promise.reject({ error: 'network-error' }))
-        .then(response => {
-            if (!response.ok) {
-                return response.json().then(err => Promise.reject(err));
-            }
-            return response.json();
-        })
 };
 
 export function fetchSaveAlbum(albumInfo) {
-    return fetch('/api/v1/userLibrary/albums', {
+    return fetchRequest(`${api}/userLibrary/albums`, {
         method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-        },
+        headers: headers,
         credentials: 'include',
         body: JSON.stringify({ albumInfo }),
     })
-        .catch(err => Promise.reject({ error: 'network-error' }))
-        .then(response => {
-            if (!response.ok) {
-                return response.json().then(err => Promise.reject(err));
-            }
-            return response.json();
-        })
 };
 
 export function fetchDeleteAlbum(id) {
-    return fetch(`/api/v1/userLibrary/albums/${id}`, {
+    return fetchRequest(`${api}/userLibrary/albums/${id}`, {
         method: 'DELETE',
-    })
-        .catch(() => Promise.reject({ error: 'networkError' }))
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            return response.json()
-                .catch(error => Promise.reject({ error }))
-                .then(err => Promise.reject(err));
-        });
+    });
 };
 
 export function fetchAddReview(content, reviewedAlbumInfo) {
-    return fetch('/api/v1/userLibrary/reviews', {
+    return fetchRequest(`${api}/userLibrary/reviews`, {
         method: 'POST',
-        headers: {
-            'content-type': 'application/json',
-        },
+        headers: headers,
         credentials: 'include',
         body: JSON.stringify({ content, reviewedAlbumInfo }),
     })
-        .catch(err => Promise.reject({ error: 'network-error' }))
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            return response.json()
-                .catch(error => Promise.reject({ error }))
-                .then(err => Promise.reject(err));
-        });
 };
 
 export function fetchDeleteReview(id) {
-    return fetch(`/api/v1/userLibrary/reviews/${id}`, {
+    return fetchRequest(`${api}/userLibrary/reviews/${id}`, {
         method: 'DELETE',
-    })
-        .catch(() => Promise.reject({ error: 'networkError' }))
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            return response.json()
-                .catch(error => Promise.reject({ error }))
-                .then(err => Promise.reject(err));
-        });
+    });
 };
 
 export function fetchUpdateReview(id, content) {
-    return fetch(`/api/v1/userLibrary/reviews/${id}`, {
+    return fetchRequest(`${api}/userLibrary/reviews/${id}`, {
         method: 'PATCH',
-        headers: {
-            'content-type': 'application/json',
-        },
+        headers: headers,
         credentials: 'include',
         body: JSON.stringify({ content }),
-    })
-        .catch(() => Promise.reject({ error: 'networkError' }))
-        .then(response => {
-            if (response.ok) {
-                return response.json();
-            }
-            return response.json()
-                .catch(error => Promise.reject({ error }))
-                .then(err => Promise.reject(err));
-        });
+    });
 };

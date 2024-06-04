eb237a9556693368ab9f8a1abeea2082807beb7d
xwshiba <xw2357@columbia.edu>
2024-06-03 23:23:10 -0700

    This commit revised next.config.mjs for vercel frontend deployment

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    distDir: './dist', // Changes the build output directory to `./dist/`.
+    distDir: '.next', // Use `.next` for production builds on Vercel
     images: { unoptimized: true }, // Disables image optimization as image API is not available in export mode.
     async rewrites() {
         return [
             {
                 source: '/api/:path*',
-                destination: 'http://localhost:4000/api/:path*' // Proxy to Backend
+                destination: process.env.API_URL || 'http://localhost:4000/api/:path*' // Proxy to Backend
             }
         ];
     }
